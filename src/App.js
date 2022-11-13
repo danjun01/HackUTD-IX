4189ca6b025bd0f926b3c3b038755d754f05eef9
nasuno <nasuno@nasunos-Laptop.local>
2022-11-13 04:30:48 -0600

    holy

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
         {
             buildings: "ECSW - Engineering And Computer Science West",
             floorRoom: "1.130",
-            available: "true",
+            available: "false",
         },
         {
             buildings: "ECSW - Engineering And Computer Science West",
@@ -69,7 +69,7 @@ function App() {
 
         for (let j = 0; j < curBuilding.rooms.length; j++) {
             let curRoom = rooms[j];
-            if (curRoom.available) count += 1;
+            if (curRoom.available == "true") count += 1;
         }
         newData[i].available = count;
     }
@@ -78,12 +78,27 @@ function App() {
     //   {buildings: "name", count: 1, rooms: [{floorRoom: "3.810", available: true}]}
 
     const [rowData] = useState(newData);
+    const [roomRowData, setRoomRowData] = useState();
 
     const [query, setQuery] = useState("");
 
     const [columnDefs, setColumnDefs] = useState([
         { field: "buildings" },
         { field: "available", sortable: true },
+    ]);
+
+    const availablityFormatter = (text) => {
+        if (text.value == "true") return "Available";
+        else return "Occupied";
+    };
+
+    const [roomColumnDefs, setRoomColumnDefs] = useState([
+        { field: "floorRoom", headerName: "Room Number" },
+        {
+            field: "available",
+            valueFormatter: availablityFormatter,
+            sortable: true,
+        },
     ]);
 
     const onFilterTextBoxChanged = useCallback(() => {
@@ -117,6 +132,7 @@ function App() {
     const onSelectionChanged = useCallback(() => {
         const selectedRow = gridRef.current.api.getSelectedRows();
         setSelectedRow(selectedRow);
+        setRoomRowData(selectedRow[0].rooms);
         togglePopup();
     }, []);
 
@@ -150,7 +166,7 @@ function App() {
                                 // id="search"
                                 name="search"
                                 className="block w-full rounded-md border border-gray-300 bg-white py-2 pl-[45px] pr-3 leading-5 placeholder-gray-500 focus:border-indigo-500 focus:placeholder-gray-400 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm"
-                                placeholder="Search by: Building Name, Keyword, Etc"
+                                placeholder="Building Name"
                                 type="search"
                                 id="filter-text-box"
                                 onChange={(e) => {
@@ -175,20 +191,8 @@ function App() {
                         onFirstDataRendered={autoSizeAll}
                         cacheQuickFilter={true}
                         rowSelection={"single"}
-                        onSelectionChanged={onSelectionChanged}
+                        onRowSelected={onSelectionChanged}
                     ></AgGridReact>
-
-                    {isOpen && (
-                        <Popup
-                            content={
-                                <>
-                                    <h1>{selectedRow[0].rooms[0].floorRoom}</h1>
-                                    <h1>{selectedRow[0].rooms[0].available}</h1>
-                                </>
-                            }
-                            handleClose={togglePopup}
-                        />
-                    )}
                 </div>
 
                 <img
@@ -198,6 +202,53 @@ function App() {
                     alt="placeholder-image"
                 />
             </div>
+            {isOpen ? (
+                <>
+                    <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+                        <div className="relative w-[500px] my-6 mx-auto max-w-3xl">
+                            {/*content*/}
+                            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                                {/*header*/}
+                                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                                    <h3 className="text-3xl font-semibold">
+                                        {selectedRow[0].buildings}
+                                    </h3>
+                                    <button
+                                        className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                                        onClick={() => setIsOpen(false)}
+                                    >
+                                        <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none"></span>
+                                    </button>
+                                </div>
+                                {/*body*/}
+                                <div className="relative p-6 flex items-center justify-center">
+                                    <div
+                                        className="ag-theme-alpine"
+                                        style={{ height: 550, width: 400 }}
+                                    >
+                                        <AgGridReact
+                                            rowData={roomRowData}
+                                            columnDefs={roomColumnDefs}
+                                            onFirstDataRendered={autoSizeAll}
+                                        ></AgGridReact>
+                                    </div>
+                                </div>
+                                {/*footer*/}
+                                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                                    <button
+                                        className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                        type="button"
+                                        onClick={() => setIsOpen(false)}
+                                    >
+                                        Close
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                </>
+            ) : null}
         </div>
     );
 }
